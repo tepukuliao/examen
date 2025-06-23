a6dac187941cac64972a4b6d6ee951abe91763f7
tomas vega <106896470+tepukuliao@users.noreply.github.com>
2025-06-23 09:05:25 -0400

    Update views.py

--- examen/main/views.py
+++ examen/main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth import login, logout, authenticate <-----🐢
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
